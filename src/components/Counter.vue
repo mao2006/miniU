9b4e4efdf498c3bbc9e950ba85ae35f8d9173574
xixiibn5100 <1584914306@qq.com>
2024-11-13 17:29:04 +0800

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <view>
+    <text class="title">{{ counter.count }}</text>
+    <view class="button" @tap="onAdd">ADD</view>
+  </view>
+</template>
+
+<script setup="ts">
+import { useCounterStore } from '@/stores/counter'
+const counter = useCounterStore()
+
+const onAdd = () => {
+  counter.count++
+}
+</script>
+
+<style>
+.title {
+  font-size: 32px;
+}
+.button {
+  border: 1px solid lightgray;
+  padding: 5px 10px;
+}
+</style>
